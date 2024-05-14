070450f9f814d482e5cea7aee8d32932f42a91a0
itsqubeil <daffaiannabil28@gmail.com>
2024-05-14 20:27:17 +0700

    update register

--- app/src/main/java/com/mobelprogeming/lafyuudaffaian/index.kt
+++ app/src/main/java/com/mobelprogeming/lafyuudaffaian/index.kt
@@ -1,37 +1,20 @@
 package com.mobelprogeming.lafyuudaffaian
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-
-class splashac : AppCompatActivity() {
+class index : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splashac)
+        setContentView(R.layout.activity_index)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-        }
-        Handler(Looper.getMainLooper()).postDelayed({
-            goTologinact()
-        },3000L)
-
-    }
-        private fun goTologinact() {
-            Intent(this, Loginact::class.java).also {
-                startActivity(it)
-                finish()
-
-
         }
     }
 }
--- app/src/main/java/com/mobelprogeming/lafyuudaffaian/loginact.kt
+++ app/src/main/java/com/mobelprogeming/lafyuudaffaian/loginact.kt
@@ -1,12 +1,14 @@
 package com.mobelprogeming.lafyuudaffaian
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class loginact : AppCompatActivity() {
+class Loginact : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +17,11 @@ class loginact : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val reg = findViewById<TextView>(R.id.log)
+        reg.setOnClickListener{
+            val intent = Intent(this, registeract::class.java)
+            startActivity(intent)
         }
     }
 }
